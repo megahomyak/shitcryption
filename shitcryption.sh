f71f19440e572e1efcace81a60f866468c7225f0
megahomyak <g.megahomyak@gmail.com>
2025-02-13 00:41:03 +0500

    Done-zo

---
+++ shitcryption.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+set -e
+
+err() {
+    echo "$1" >&2
+    exit 1
+}
+
+checkvar() {
+    if [ "${!1}" = "" ]; then
+        err "$2"
+    fi
+}
+
+checkvar TARGET 'The $TARGET path is not provided.'
+
+PASSWORD_FILE_PATH="$TARGET.password"
+ARCHIVE_PATH="$TARGET.tar"
+
+if [ -d "$TARGET" ]; then
+    if [ -f "$ARCHIVE_PATH" ]; then
+        err 'Both $TARGET and archive exist. This is unexpected. Stopping.'
+    fi
+    PASSWORD="$(cat "$PASSWORD_FILE_PATH" 2>/dev/null || true)"
+    if [ "$PASSWORD" = "" ]; then
+        read -p "Password: " PASSWORD
+    fi
+    if tar -I 'zstd -19' -cf - -C "$TARGET" . | gpg --batch --yes --passphrase "$PASSWORD" --symmetric --cipher-algo AES256 --output "$ARCHIVE_PATH" - ; then
+        rm -r "$TARGET"
+        rm "$PASSWORD_FILE_PATH" 2>/dev/null || true
+        echo "Encrypted successfully"
+    else
+        rm -r "$ARCHVIE_PATH"
+    fi
+else
+    if [ ! -f "$ARCHIVE_PATH" ]; then
+        err 'No $TARGET or respective archive found.'
+    fi
+    read -s -p "Password: " PASSWORD
+    mkdir -p "$TARGET"
+    if gpg --batch --yes --passphrase "$PASSWORD" --output - --decrypt "$ARCHIVE_PATH" | tar -I zstd -xf - -C "$TARGET" ; then
+        rm -r "$ARCHIVE_PATH"
+        echo "$PASSWORD" > "$PASSWORD_FILE_PATH"
+        echo "Decrypted successfully"
+    else
+        rm -r "$TARGET"
+    fi
+fi
